cb2eb86b4ba1f0a159faeeaed907651796926479
Eric Emery <ericemery099@gmail.com>
2023-08-04 09:21:03 +0000

    added models

--- djangojams/app/models.py
+++ djangojams/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Songs(models.Model):
+    title = models.Charfield(max_length=200)
+    artist = models.ManyToManyField('Artists')
+    album = models.Charfield(max_length=200)
+    genre = models.Charfield(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Artists(models.Model):
+    name = models.Charfield(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Albums(models.Model):
+    label = models.Charfield(max_length=200)
