7dee7466bf6331237dcb86cd915419664f8bf183
Eric Emery <ericemery099@gmail.com>
2023-08-04 14:43:22 +0000

    fixed database connection issue

--- djangojams/app/models.py
+++ djangojams/app/models.py
@@ -3,35 +3,35 @@ from django.db import models
 # Create your models here.
 
 class Songs(models.Model):
-    title = models.Charfield(max_length=200)
+    title = models.CharField(max_length=200)
     artist = models.ManyToManyField('Artists')
-    album = models.Charfield(max_length=200)
-    genre = models.ManytoManyfield('Genres')
+    album = models.CharField(max_length=200)
+    genre = models.ManyToManyField('Genres')
 
     def __str__(self):
         return self.title
 
 class Artists(models.Model):
-    name = models.Charfield(max_length=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 class Albums(models.Model):
-    label = models.Charfield(max_length=200)
+    label = models.CharField(max_length=200)
 
     def __str__(self):
         return self.label
 
 class Genres(models.Model):
-    label = models.Charfield(max_length=200)
+    label = models.CharField(max_length=200)
 
     def __str__(self):
         return self.label
 
 class Users(models.Model):
-    email = models.Charfield(max_length=200)
-    username = models.Charefield(max_length=200)
+    email = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
 
     def __str__(self):
         return self.username
